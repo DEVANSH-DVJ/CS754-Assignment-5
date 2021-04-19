68162fcac16f0b0ab3a1d6b7d7f492f2f236e686
devansh-dvj <devansh.dvj@gmail.com>
2021-04-19 05:44:10 +0530

    Q2: Added comments to code

--- MATLAB/q2.m
+++ MATLAB/q2.m
@@ -9,6 +9,11 @@ tol = 0.1;
 rmse_avg = [];
 
 for alpha=[0, 3]
+    % Generating a random orthonormal matrix
+    % The n-dimensional normal distribution has spherical symmetry,
+    % which implies that after normalization the drawn vectors would be
+    % uniformly distributed on the n-dimensional unit sphere.
+    % Then using Gram-schmidt on the random vector to generate matrix
     U = zeros(n);
     vi = randn(n,1);
     U(:,1) = vi ./ norm(vi);
@@ -22,30 +27,39 @@ for alpha=[0, 3]
       U(:,i) = vi ./ nrm;
     end
 
+    % Generating Co-variance Matrix
     Sigma = U * diag((1:n).^(-alpha)) * U';
 
+    % Generating 10 n-dimensional vectors
     xs = mvnrnd(zeros(n,1), Sigma, 10)';
 
     for m=[40, 50, 64, 80, 100, 120]
         rmse = zeros(10, 1);
+
+        % Generating a random sensing matrix
         phi = randn(m, n) / sqrt(m);
 
+        % Variables used in reconstruction (for faster computation)
         SpT = Sigma * phi';
         pSpT = phi * SpT;
 
         for i=1:10
+            % Generating noisy measurement
             y = phi * xs(:, i);
             sig = 0.01 * mean(abs(y));
             y = y + mvnrnd(zeros(m, 1), eye(m)*sig^2, 1)';
+
+            % Reconstruction of x
             x = (SpT - SpT/(eye(m)*sig^2 + pSpT)*pSpT) * y / (sig^2);
 
+            % Calculating RMSE
             rmse(i) = sqrt(mean((x - xs(:,i)).^2))/sqrt(mean(xs(:,i).^2));
         end
         rmse_avg = [rmse_avg mean(rmse)];
     end
 end
 
-% average RMSE vs m
+% Average RMSE vs m
 figure;
 
 plot([40, 50, 64, 80, 100, 120], rmse_avg(1:6), 'r');
